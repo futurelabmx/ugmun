579d4fd3dc7c5b36958fe0fb8c756d060f36913c
Rodolfo Ferro <rodolfoferroperez@gmail.com>
2020-01-27 15:18:31 -0600

    Modified main.js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 // DOM HTML Back-end
-const express = require('express');
+import express from 'express';
+
 const app = express();
 
 app.get('/', (req, res) => {
@@ -12,29 +13,4 @@ app.get('/acerca-de/', (req, res) => {
 
 app.get('/acercade/', (req, res) => {
   res.sendFile('../acerca_de.html', { root: __dirname });
-});
-
-app.get('/faq/', (req, res) => {
-  res.sendFile('./faq.html', { root: __dirname });
-});
-
-app.get('/code-of-conduct/', (req, res) => {
-  res.sendFile('./code-of-conduct.html', { root: __dirname });
-});
-
-// Speakers-Pointers
-app.get('/speakers/benjamin/', (req, res) => {
-  res.sendFile('./speakers-benjamin.html', { root: __dirname });
-});
-
-app.get('/speakers/alejandro/', (req, res) => {
-  res.sendFile('./speakers-alejandro.html', { root: __dirname });
-});
-
-app.get('/speakers/gustavo/', (req, res) => {
-  res.sendFile('./speakers-gustavo.html', { root: __dirname });
-});
-
-app.get('/speakers/andres/', (req, res) => {
-  res.sendFile('./speakers-andres.html', { root: __dirname });
 });
